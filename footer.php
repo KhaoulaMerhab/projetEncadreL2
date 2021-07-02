717090ac876986d8ee6fa55d80d452e86da35afe
khaouloula <khaoulamerhab28@gmail.com>
2021-07-02 18:33:30 +0100

    add galleryPage and make final changes on home page and galleryPage

---
+++ footer.php
@@ -0,0 +1,87 @@
+<!--**footer-upSection goes here-->
+<footer class="footer">
+        <!--**big upSection container-->
+        <div class="footer__upSection upSec-md upSec-sm">
+            
+            <!--?left logo container-->
+            <div class="logoContainer">
+                <div class="logoImg">
+                    <img src="imgs/LOGO.PNG" alt="footer logo">
+                </div>
+                <div class="slogan">
+                    <p>
+                        Smudges , colors , Creativity .. Artemis home of art .
+                    </p>
+                </div>
+            </div>
+
+            <!--?links container-->
+            <div class="linksContainer">
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="gallery.html">Gallery</a></li>
+                    <li><a href="blog.html">Blog</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+
+            <!--?similar links container-->
+            <div class="similarLinksContainer">
+                <ul>
+                    <li><a href="https://unsplash.com/">unsplash.com</a></li>
+                    <li><a href="https://www.pinterest.com/">pinterest.com</a></li>
+                    <li><a href="https://dribbble.com/">dribbble.com</a></li>
+                    <li><a href="https://www.behance.net/">behance.net</a></li>
+                </ul>
+            </div>   
+                
+            <!--?contact container-->
+            <div class="contactContainer">
+                <div class="contactContainer--up">
+                    <div class="call flex flex-ai-c">
+                        <div class="phoneIcon">
+                            <i class="fas fa-phone"></i>
+                        </div>
+                        <div class="num">
+                            <p>
+                                123456789
+                            </p>
+                        </div>
+                    </div>
+                    <div class="mail flex flex-ai-c">
+                        <div class="emailIcon">
+                            <i class="fas fa-envelope"></i>
+                        </div>
+                        <div class="email">
+                            <p>
+                                Artemis@gmail.com
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="contactContainer--down flex flex-ai-c flex-jc-sb">
+                    <div class="fbIcon">
+                        <i class="fab fa-facebook-f"></i>
+                    </div>
+                    <div class="twIcon">
+                        <i class="fab fa-twitter"></i>
+                    </div>
+                    <div class="inIcon">
+                        <i class="fab fa-instagram"></i>
+                    </div>
+                    <div class="psIcon">
+                        <i class="fab fa-pinterest-p"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <!--**small downSection container-->
+        <div class="footer__downSection flex flex-jc-c flex-ai-c">
+            <p>
+               &copy; All Copyrights Reserved 2021
+            </p>
+        </div>
+    </footer>
+    <!--**footer-upSection finishes here-->
